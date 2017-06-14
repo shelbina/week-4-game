69194633aa12cba5a774dd12dfb988792727c5e5
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-06-13 23:18:02 -0400

    create end game and reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,13 @@
 $(document).ready(function(){
 
 var wins=0;
+	$('#yourWins').text(wins);
 var loses=0;
+	$('#yourLoses').text(loses);
 var ranGem=0;
 var yourGem=0;
+
+
 
 
 //generate random gem power
@@ -16,35 +20,54 @@ var num1 = Math.floor(Math.random() * 11 + 1);
 	$("#red").on("click", function() {
 	yourGem+=num1;
 	$("#yourGem").text(yourGem);
+	endGame();
 	    });
 
 var num2 = Math.floor(Math.random() * 11 + 1);
 	$("#green").on("click", function() {
 	yourGem+=num2;
 	$("#yourGem").text(yourGem);
+	endGame();
 	    });
 
 var num3 = Math.floor(Math.random() * 11 + 1);
 	$("#yellow").on("click", function() {
 	yourGem+=num3;
 	$("#yourGem").text(yourGem);
+	endGame();
 	    });
 
 var num4 = Math.floor(Math.random() * 11 + 1);
 	$("#blue").on("click", function() {
 	yourGem+=num4;
 	$("#yourGem").text(yourGem);
+	endGame();
 	    });
 
+//create win lose to end game
 function endGame(){
-	if (ranGem==yourGem){
+	if (yourGem==ranGem){
 		wins++;
+		$("#yourWins").text(wins);
+		reset();
+	}
+	else if (yourGem > ranGem){
+		loses++;
+		$("#yourLoses").text(loses);
+		reset();
+	};
+};
 
 
-}
+// reset the game
+function reset(){
 
+	randomNum = Math.floor(Math.random() * 101 + 19);
 
-
+	num1 = Math.floor(Math.random() * 11 + 1);
+	num2 = Math.floor(Math.random() * 11 + 1);
+	num3 = Math.floor(Math.random() * 11 + 1);
+	num4 = Math.floor(Math.random() * 11 + 1);
 };
 
 });
