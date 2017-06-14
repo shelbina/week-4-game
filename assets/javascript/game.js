572ad166103fdefc5a13fc729e095995bbdf3725
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-06-13 20:22:01 -0400

    generate random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,50 @@
+//initialize game
+$(document).ready(function(){
+
 var wins=0;
 var loses=0;
+var ranGem=0;
+var yourGem=0;
+
+
+//generate random gem power
+var randomNum = Math.floor(Math.random() * 101 + 19);
+$("#ranGem").text(randomNum);
+
+//generate random number for pictures and create on click function to connect picture to random number
+var num1 = Math.floor(Math.random() * 11 + 1);
+	$("#red").on("click", function() {
+	yourGem+=num1;
+	$("#yourGem").text(yourGem);
+	    });
+
+var num2 = Math.floor(Math.random() * 11 + 1);
+	$("#green").on("click", function() {
+	yourGem+=num2;
+	$("#yourGem").text(yourGem);
+	    });
+
+var num3 = Math.floor(Math.random() * 11 + 1);
+	$("#yellow").on("click", function() {
+	yourGem+=num3;
+	$("#yourGem").text(yourGem);
+	    });
+
+var num4 = Math.floor(Math.random() * 11 + 1);
+	$("#blue").on("click", function() {
+	yourGem+=num4;
+	$("#yourGem").text(yourGem);
+	    });
+
+function endGame(){
+	if (ranGem==yourGem){
+		wins++;
+
+
+}
 
 
 
-    $(document).ready(function() {
-      //When random button is clicked...
-     $('#random-button').on("click", function() {
+};
 
-      ///...we generate  a random number
-         var random = Math.floor(Math.random() * 1000) + 1;
-
-      ///...and then dumb the random nubmer into our random-number div.
-         $("#random-number").html(random);
-      });
-      // ...
-
-    });
-
-function startGame() {
-
-    numGuesses = 11;
-
-    chosenWord = mysteryWord[Math.floor(Math.random() * mysteryWord.length)];
-    lettersInChosenWord = chosenWord.split("");
-    numBlanks = lettersInChosenWord.length;
-
-
-    console.log(mysteryWord);
-    blanksAndSuccesses = [];
-    wrongGuesses = [];
-
-
-    for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("_");
-    }
-
-    console.log(blanksAndSuccesses);
-
-
-    document.getElementById("guessesLeft").innerHTML = numGuesses;
+});
