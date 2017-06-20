3ed28dbdfa2076c564210dc4ea721ab36ba15735
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-06-20 19:04:22 -0400

    replace ranGem with randomNum in function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,45 @@
 $(document).ready(function(){
 
 var wins=0;
-	$('#yourWins').text(wins);
+    $('#yourWins').text(wins);
 var loses=0;
-	$('#yourLoses').text(loses);
+    $('#yourLoses').text(loses);
 var ranGem=0;
 var yourGem=0;
 
 //generate random number for pictures and create on click function to connect picture to random number
 var num1 = Math.floor(Math.random() * 11 + 1);
-	$("#red").on("click", function() {
-	yourGem+=num1;
-	$("#yourGem").text(yourGem);
-	endGame();
-	reset();
-	    });
+    $("#red").on("click", function() {
+    yourGem += num1;
+    $("#yourGem").text(yourGem);
+    console.log('ranGem: '+ ranGem +" yourGem"+yourGem);
+    endGame();
+    //
+        });
 
 var num2 = Math.floor(Math.random() * 11 + 1);
-	$("#green").on("click", function() {
-	yourGem+=num2;
-	$("#yourGem").text(yourGem);
-	endGame();
-	reset();
-	    });
+    $("#green").on("click", function() {
+    yourGem+=num2;
+    $("#yourGem").text(yourGem);
+    endGame();
+    //reset();
+        });
 
 var num3 = Math.floor(Math.random() * 11 + 1);
-	$("#yellow").on("click", function() {
-	yourGem+=num3;
-	$("#yourGem").text(yourGem);
-	endGame();
-	reset();
-	    });
+    $("#yellow").on("click", function() {
+    yourGem+=num3;
+    $("#yourGem").text(yourGem);
+    endGame();
+    //reset();
+        });
 
 var num4 = Math.floor(Math.random() * 11 + 1);
-	$("#blue").on("click", function() {
-	yourGem+=num4;
-	$("#yourGem").text(yourGem);
-	endGame();
-	reset();
-	    });
+    $("#blue").on("click", function() {
+    yourGem+=num4;
+    $("#yourGem").text(yourGem);
+    //endGame();
+    reset();
+        });
 
 
 
@@ -48,35 +49,38 @@ var randomNum = Math.floor(Math.random() * 101 + 19);
 $("#ranGem").text(randomNum);
 
 //game continues if yourgem is less than random
-function game(){
-	if (yourGem < ranGem){
-};
+//function game(){
+ //   if (yourGem < ranGem){
+        console.log('game is talking')
+//};
 
 //end game with lose if yourgem is more than random gem, with win if your gem is = to randomgem
 function endGame(){
-	if (yourGem==ranGem){
-		wins++;
-		$("#yourWins").text(wins);
-		reset();
-	}
-	if (yourGem > ranGem){
-		loses++;
-		$("#yourLoses").text(loses);
-		reset();
-	}
-	};;
-};
+    if (yourGem==randomNum){
+        wins++;
+        $("#yourWins").text(wins);
+        console.log('wins' +wins);
+        reset();
+    }
+    if (yourGem > randomNum){
+        loses++;
+        $("#yourLoses").text(loses);
+        console.log('Losses'+loses);
+        reset();
+    }
+    };
+//};
 
 
 // reset the game
 function reset(){
 
-	randomNum = Math.floor(Math.random() * 101 + 19);
+   randomNum = Math.floor(Math.random() * 101 + 19);
 
-	num1 = Math.floor(Math.random() * 11 + 1);
-	num2 = Math.floor(Math.random() * 11 + 1);
-	num3 = Math.floor(Math.random() * 11 + 1);
-	num4 = Math.floor(Math.random() * 11 + 1);
+   num1 = Math.floor(Math.random() * 11 + 1);
+    num2 = Math.floor(Math.random() * 11 + 1);
+    num3 = Math.floor(Math.random() * 11 + 1);
+    num4 = Math.floor(Math.random() * 11 + 1);
 };
 
 });
